e0dd1c55a1258fb6b2fc66ed0dafd46f40f6d2b9
Raj Sandhu <rajsandhu@gmail.com>
2020-09-07 20:18:15 +0200

    tada

---
+++ src/FirstComponent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// class FirstComponent extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+// }
+
+const FirstComponent = () => {
+
+    return (
+        <div>
+            <p>hello Kat</p>
+            <p>hello again</p>
+        </div>
+    )
+
+}
+
+export default FirstComponent
